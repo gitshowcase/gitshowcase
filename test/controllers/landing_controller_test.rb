31cfdf6ecaf2499ddd9ff26ff5f749beac150bc8
Jônatas Rancan <jonatasrancan@gmail.com>
2017-01-30 15:08:03 -0200

    Remove test folder

--- test/controllers/landing_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LandingControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/factories/users.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    
-  end
-end
