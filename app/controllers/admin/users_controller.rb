6da63b21b71a6ec77766bcd7632f37e286231675
Pedro S. Moreira <pedsmoreira@gmail.com>
2017-04-17 14:58:52 -0300

    Fixing admin user pagination

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < AdminController
   # GET /admin/users
   def index
-    @users = User.order('id DESC').paginate(page: params[:page], per_page: 3)
+    @users = User.order('id DESC').paginate(page: params[:page], per_page: 10)
   end
 end
